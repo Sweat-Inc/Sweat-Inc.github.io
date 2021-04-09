0562cb365cf678f0c4457aeb2a9b8b52b89f5d57
William Fedele <47335043+william-fedele@users.noreply.github.com>
2021-04-08 22:01:32 -0400

    Added CLI Effect (#3)
    * Homepage title
    
    * Added footer and newline to imitate CLI.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-let title = "Sweat, Inc"
+let title = "Sweat, Inc";
 let i = 0;
 setInterval(function() {
     let cursor = (i%4==0 || i%3==0 ? "_" : "&nbsp;");
-    let displayText = title.substring(0, i);
+    let displayText = title.substring(0, i) + (title.length - i < 0 ? "<br>>>" : "");
     document.getElementById("companyName").innerHTML = displayText + cursor;
     i++;
 
